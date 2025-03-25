c7e8ece90a91d490260266beab02e57d51258a47
Jaydeep Panwar <Jaydeeppanwar@gmail.com>
2025-03-25 16:32:09 +0530

    modified some files

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,7 +6,7 @@ const userRouter=express.Router()
 
 userRouter.route("/newUser").post(newUser as RequestHandler);
 userRouter.route('/all').get(adminAuthentication as RequestHandler,getAllUser as RequestHandler);
-userRouter.route('/:id').get(adminAuthentication as RequestHandler,getUserById as RequestHandler);
+userRouter.route('/:id').get(getUserById as RequestHandler);
 userRouter.route('/:id').delete(adminAuthentication as RequestHandler,deleteUser as RequestHandler);
 userRouter.route('/update/user/:id').patch(updateUser as RequestHandler);
 
