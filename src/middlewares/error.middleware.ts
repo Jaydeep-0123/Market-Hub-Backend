98d5c03237e9e94254af854044a5612ef5d0dcd3
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2025-04-28 22:20:30 +0530

    Update error.middleware.ts

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,24 +1,26 @@
-import { Request, Response, NextFunction} from "express";
-import ErrorHandler from "../utils/utility-class.js";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+// import ErrorHandler from "../utils/utility-class.js";
 import { ControllerType } from "../types/types.js";
 
-export const errorMiddleware = (
-  err: ErrorHandler,
-  req: Request,
-  res: Response,
-  next: NextFunction
+export const errorMiddleware: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next
 ) => {
   err.message ||= "Internal Server Error";
   err.statusCode ||= 500;
- if(err.name==="CastError")
- {
-  err.message="Invalid Id"
- }
-  return res.status(err.statusCode).json({
+
+  if (err.name === "CastError") {
+    err.message = "Invalid Id";
+  }
+
+  res.status(err.statusCode).json({
     status: "Failed",
     message: err.message,
   });
 };
+
 
 export const TryCatch =
   (func: ControllerType) =>
